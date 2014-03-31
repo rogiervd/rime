8073db4fbab87554416c0d14943b2d775e4a1c00
Rogier van Dalen <rogiervd@gmail.com>
2014-03-31 08:59:28 +0100

    Use boost_unit_test.hpp from the Utility library
    Ignore-this: 2c947996c888cd160d91ea7e1e110e6e
    This replaces a file specific to this project.
    
    darcs-hash:20140331075928-c9a90-a6869eebb07568ef338bcdbb0b6738352007d339

--- test/rime/cast.cpp
+++ test/rime/cast.cpp
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #define BOOST_TEST_MODULE test_rime_cast
-#include "../boost_unit_test.hpp"
+#include "utility/test/boost_unit_test.hpp"
 
 #include "rime/cast.hpp"
 
--- test/rime/if.cpp
+++ test/rime/if.cpp
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #define BOOST_TEST_MODULE test_rime_if
-#include "../boost_unit_test.hpp"
+#include "utility/test/boost_unit_test.hpp"
 
 #include "rime/if.hpp"
 
