d9d1d9b904d46e92a88349a2552f5db89a5fe121
Rogier van Dalen <rogiervd@gmail.com>
2014-11-18 16:37:22 +0000

    Remove outdated reference to meta/merge_types.hpp
    Ignore-this: ae8d9bddf16d344317805f2f10c8daf2
    
    darcs-hash:20141118163722-c9a90-728be36f403985e96dcbd93eefaab9190d2aaa3f

--- include/rime/detail/variant_fwd.hpp
+++ include/rime/detail/variant_fwd.hpp
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "utility/storage.hpp"
 #include "meta/contains.hpp"
-#include "meta/merge_types.hpp"
 
 namespace rime {
 
