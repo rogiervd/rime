898791e4878f54b86532d2f55d7386bbeddac0d8
Rogier van Dalen <rogiervd@users.noreply.github.com>
2026-09-22 22:29:57 +0100

    Build and test under Bazel (#3)
    Also use Bazel in CI

--- test/rime/test-enable_if_constant.cpp
+++ test/rime/test-enable_if_constant.cpp
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 #define BOOST_TEST_MODULE test_rime_enable_if_constant
-#include "utility/test/boost_unit_test.hpp"
+#include <boost/test/unit_test.hpp>
 
 #include "rime/enable_if_constant.hpp"
 
